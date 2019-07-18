6f20bc25676df84a8bdbb1373c2a1333f9fb7947
Don Kimberlin <donkimberlin@hotmail.com>
2019-07-18 01:44:57 -0700

    Update UserData.java

--- app/src/main/java/anticlimacticteleservices/clienttest26/UserData.java
+++ app/src/main/java/anticlimacticteleservices/clienttest26/UserData.java
@@ -3,17 +3,30 @@ package anticlimacticteleservices.clienttest26;
 public class UserData {
     List<Channel> channels = new ArrayList<>();
     List<String> feedLinks = new ArrayList<>;
-    
+    SharedPreferences prefs
+    SharedPreferences.Editor editor
+        
     public List<String> getFeedLinks(){
-      return feedLinks;
+        prefs = this.getSharedPreferences( "com.mycompany.client", Context.MODE_PRIVATE);
+        editor = prefs.edit();
+        Set<String> feeds = prefs.getStringSet("channelUrls",null);
+        if (feeds.isEmpty())
+            feeds.add("https://www.youtube.com/feeds/videos.xml?channel_id=UC-lHJZR3Gqxm24_Vd_AJ5Yw")
+            feeds.add("https://bitchute.com/channel/Styxhexenham/");
+        }
+        feedLinks=feeds;
+      return "done";
     }
     public void removeFeedLink(String deadLink){
-      // figure out best way to remove feedlink from extent list
+      //TODO figure out best way to remove feedlink from extent list
     }
     public void addFeedLink(String newLink){
       //TODO duplicate and viability parsing.
       feedLinks.add(newLink);
+      prefs = this.getSharedPreferences( "com.mycompany.client", Context.MODE_PRIVATE);
+      editor = prefs.edit();
+      editor.putStringSet("channelUrls", feedLinks);
+      editor.commit();  
     }
-    
 }
    
